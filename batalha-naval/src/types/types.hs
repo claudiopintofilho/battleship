8adb981b370c6fb96ef2ff022de8ccdb9511f7c3
claudiopintofilho <claudio.pinto.lopes.filho@ccc.ufcg.edu.br>
2026-05-26 20:17:37 -0300

    Teste

--- batalha-naval/src/types/types.hs
+++ batalha-naval/src/types/types.hs
@@ -1,3 +1,11 @@
-main :: IO ()
-main = do
-  putStrLn "Batalha Naval Protótipo"
+module Types where
+--Determinando um tipo de "Coordenada"
+type Coordenada = (Int, Int)
+
+--Estados de cada celula
+data Celula = Agua
+            | Navio
+            | Atingido
+            | Erro
+            deriving(Eq, Show)
+
