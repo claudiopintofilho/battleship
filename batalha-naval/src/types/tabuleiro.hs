250097a3838bbf0e37cabc12092d116d507ca751
Pedroct06 <pedro.henrique.coelho.torres@ccc.ufcg.edu.br>
2026-05-26 21:46:30 -0300

    adicionando os tipos e o tabuleiro

--- batalha-naval/src/types/tabuleiro.hs
+++ batalha-naval/src/types/tabuleiro.hs
@@ -1,8 +1,5 @@
-module Types where
+module Main where
 import System.IO
---Determinando um tipo de "Coordenada"
-type Coordenada = (Int, Int)
-
 -- criando as células para serem usadas no tabuleiro
 data Cell = Agua | Navio | Acertou | Errou
 
@@ -34,23 +31,4 @@ printTabuleiro [] [] n = return ()
 printTabuleiro (h:t) (h2:t2)n = do
     putStrLn (show n ++ "  " ++ converte h ++ "      " ++ converte h2)
     printTabuleiro t t2 (n+1)
-
-
-data Navios = Navios {
-    tipo :: String,
-    tamanho :: Int,
-    posicoes :: [Coordenada],
-    partesAtingidas :: [Coordenada]
-} deriving (Show, Eq)
-
-naviosPadrao :: [Navios]
-naviosPadrao = [
-    Navios "Grande" 3 [] [],
-    Navios "Medio" 2 [] [],
-    Navios "Pequeno" 1 [] []
-]
-
--- Definindo a orientacao do navio 
-data Orientacao  = H | V deriving (Show, Eq)
-
 
