1c12c237b20c978e248934f78235e3d0f68bc51f
Pedroct06 <pedro.henrique.coelho.torres@ccc.ufcg.edu.br>
2026-07-29 15:41:25 -0300

    Modificando os emojis do tabuleiro

--- prolog/batalha-naval/interface/interface.pl
+++ prolog/batalha-naval/interface/interface.pl
@@ -19,10 +19,10 @@
 
 % make_cell(+Celula, -Simbolo)
 % Equivalente a "makeCell"
-make_cell(agua,    '~ ').
-make_cell(navio,   'B ').
-make_cell(acertou, 'X ').
-make_cell(errou,   'o ').
+make_cell(agua,    '🌊').
+make_cell(navio,   '🛥️').
+make_cell(acertou, '💥').
+make_cell(errou,   '💨').
 
 % converte_linha(+Linha, -Texto)
 % Equivalente a "converteLinha linha = concatMap makeCell linha"
